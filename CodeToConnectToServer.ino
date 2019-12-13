b6d8d8d3f006471f919df30caae06c345d131ebd
thingimajig13 <54867822+thingimajig13@users.noreply.github.com>
2019-12-12 18:54:20 -0600

    Add files via upload
    This is the final code to connect to the server

--- CodeToConnectToServer.ino
+++ CodeToConnectToServer.ino
@@ -6,11 +6,20 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
+#include <Wire.h>
+
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
 WiFiMulti WiFiMulti;
 
 void setup()
 {
+    Wire.begin();
+    Wire.beginTransmission(MPU);
+    Wire.write(0x6B); 
+    Wire.write(0);    
+    Wire.endTransmission(true);
     Serial.begin(115200);
     Serial.print("Starting");
     delay(10);
@@ -40,9 +49,21 @@ void setup()
 
 void loop()
 {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+
    HTTPClient http;
- 
-    http.begin("http://ancient-bastion-28512.herokuapp.com/data?x=10&y=20&z=30"); //Specify the URL
+
+
+    String url  = "http://ancient-bastion-28512.herokuapp.com/data?x=" + String(AcX) + "&y=" + String(AcY) + "&z=" + String(AcZ);
+
+    Serial.println(url);
+    http.begin(url); //Specify the URL
     int httpCode = http.GET();                                        //Make the request
  
     if (httpCode > 0) { //Check for the returning code
@@ -58,63 +79,6 @@ void loop()
  
     http.end(); //Free the resources
 
-    delay(5000);
-  
-    //const uint16_t port = 80;
-//const char * host = "htt"; // ip or dns
-//HttpClient http;
-//http.begin("http://ancient-bastion-28512.herokuapp.com/available");
-//http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-//http.get("http://ancient-bastion-28512.herokuapp.com/available");
-//http.writeToStream(&Serial);
-//http.end();
-//   const uint16_t port = 80;
-//   const char * host = "ancient-bastion-28512.herokuapp.com"; // ip or dns
-//
-//    Serial.print("Connecting to test ");
-//    Serial.println(host);
-//
-//    // Use WiFiClient class to create TCP connections
-//    WiFiClient client;
-//
-//    if (!client.connect(host, port)) {
-//        Serial.println("fffff");
-//        Serial.println("Connection failed.");
-//        Serial.println("Waiting 5 seconds before retrying...");
-//        delay(5000);
-//        return;
-//    }
-//
-//    // This will send a request to the server
-//    //uncomment this line to send an arbitrary string to the server
-//    //client.print("Send this data to the server");
-//    //uncomment this line to send a basic document request to the server
-//    client.println("GET /available HTTP/1.1\n\n");
-////    client.println("\r");
-//
-//  int maxloops = 0;
-//
-//  //wait for the server's reply to become available
-//  while (!client.available() && maxloops < 1000)
-//  {
-//    maxloops++;
-//    delay(1); //delay 1 msec
-//  }
-//  if (client.available() > 0)
-//  {
-//    //read back one line from the server
-//    String line = client.readStringUntil('\r');
-//    Serial.println("Printing response");
-//    Serial.println(line);
-//  }
-//  else
-//  {
-//    Serial.println("client.available() timed out ");
-//  }
-//
-//    Serial.println("Closing connection.");
-//    client.stop();
-//
-//    Serial.println("Waiting 5 seconds before restarting...");
-//    delay(5000);
+    delay(500);
+ 
 }
